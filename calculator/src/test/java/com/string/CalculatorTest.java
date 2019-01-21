b5501c6f02d2c5d49f9daa2940f326c15378455b
praweenku <46890038+praweenku@users.noreply.github.com>
2019-01-21 16:09:29 +0530

    Update CalculatorTest.java

--- calculator/src/test/java/com/string/CalculatorTest.java
+++ calculator/src/test/java/com/string/CalculatorTest.java
@@ -18,13 +18,28 @@ public class CalculatorTest
      * Rigourous Test :-)
      */
 	@org.junit.Test
-    public void testCalulate()
+    public void testCalulateValidExpr1()
+    {
+		Calculator calc = new Calculator();		
+        assertEquals( calc.calulate("7+(6*5^2+3-4/2)"),"158.0" );
+    }
+	@org.junit.Test
+    public void testCalulateValidExpr2()
     {
 		Calculator calc = new Calculator();
-		
-        assertEquals( calc.calulate("7+(6*5^2+3-4/2)"),"158.0" );
+        assertEquals( calc.calulate("(8*5/8)-(3/1)-5"),"-3.0" );
+    }
+	@org.junit.Test
+    public void testCalulateInValidExpr1()
+    {
+		Calculator calc = new Calculator();
         assertEquals( calc.calulate("7+(67(56*2))"),"INVALID EXPRESSION" );
         assertEquals( calc.calulate("8*+7"),"INVALID EXPRESSION" );
-        assertEquals( calc.calulate("(8*5/8)-(3/1)-5"),"-3.0" );
+    }
+	@org.junit.Test
+    public void testCalulateInValidExpr2()
+    {
+		Calculator calc = new Calculator();
+        assertEquals( calc.calulate("8*+7"),"INVALID EXPRESSION" );
     }
 }
